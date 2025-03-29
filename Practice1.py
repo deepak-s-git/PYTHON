b0a9b33fdd160ee4d9e392f8780cc3b5c477aba4
deepak-s-git <deepak.s.workdesk@gmail.com>
2025-03-29 19:46:22 +0530

    An Overview of Python

---
+++ Practice1.py
@@ -0,0 +1,45 @@
+# print("Hello World")
+
+# # AREA OF RECTANGLE 
+# length = 10
+# breadth = 20
+# area = length * breadth 
+# print(area)
+
+# #ID OF AN OBJECT
+# num1 = 10
+# print(id(num1))
+
+# #DATATYPES
+# num1 = 1204
+# var1 = True
+# float1 = -192.9
+# var2 = -3 + 7.2J
+# print(type(num1))
+# print(type(var1))
+# print(type(float1))
+# print(type(var2))
+
+# # SEQUENCES
+# list = [10,20,'Delhi','Blore']
+# print(list)
+# tuple = (10,20,'Blore')
+# print(tuple)
+
+# # SETS
+# set = {1,2,1,'Blore'}
+# print(set)
+
+# #DICTIONARIES
+# dict = {'Fruit':'Apple','Climate':'Cold','Price(KG)':120}
+# print(dict)
+# print(dict['Price(KG)'])
+
+# #INPUTS
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# print(type(age))
+
+#Typecasting
+age = int(input("Enter your age: "))
+print(type(age))
